53d6ec3023466032e20df0df19fd9f5ace876b1a
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2022-02-08 20:24:41 +0530

    Add Appconsts file

---
+++ src/main/kotlin/com/github/srilakshmikanthanp/quicknote/appconsts/AppConsts.kt
@@ -0,0 +1,31 @@
+package com.github.srilakshmikanthanp.quicknote.appconsts
+
+/**
+ * A Singleton instance that Holds the Constants of the Application
+ */
+object AppConsts {
+    /**
+     * A Constant for the Home Page of Application
+     */
+    const val APP_HOME = "https://github.com/srilakshmikanthanp/QuickNote"
+
+    /**
+     * A Constant for the Min Width of Editor
+     */
+    const val MIN_WIDTH = 100
+
+    /**
+     * A Constant for the Min Height of Editor
+     */
+    const val MIN_HEIGHT = 100
+
+    /**
+     * A Constant for the Max Width of Editor
+     */
+    const val MAX_WIDTH = 1000
+
+    /**
+     * A Constant for the Max Height of Editor
+     */
+    const val MAX_HEIGHT = 550
+}
