d3a120a44922ebea56c2fb70f97ed34bdbd43bd1
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2022-02-08 20:28:37 +0530

    Format Appconsts file

--- src/main/kotlin/com/github/srilakshmikanthanp/quicknote/appconsts/AppConsts.kt
+++ src/main/kotlin/com/github/srilakshmikanthanp/quicknote/appconsts/AppConsts.kt
@@ -7,25 +7,25 @@ object AppConsts {
     /**
      * A Constant for the Home Page of Application
      */
-    const val APP_HOME = "https://github.com/srilakshmikanthanp/QuickNote"
+    const val APP_HOME   =  "https://github.com/srilakshmikanthanp/QuickNote"
 
     /**
      * A Constant for the Min Width of Editor
      */
-    const val MIN_WIDTH = 100
+    const val MIN_WIDTH  =  100
 
     /**
      * A Constant for the Min Height of Editor
      */
-    const val MIN_HEIGHT = 100
+    const val MIN_HEIGHT =  100
 
     /**
      * A Constant for the Max Width of Editor
      */
-    const val MAX_WIDTH = 1000
+    const val MAX_WIDTH  =  1000
 
     /**
      * A Constant for the Max Height of Editor
      */
-    const val MAX_HEIGHT = 550
+    const val MAX_HEIGHT =  550
 }
